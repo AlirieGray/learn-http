e7cdfe0fcbe9d2b9c027c5ca5de7900028f04d2b
Catislin <caitlin.estrellita@gmail.com>
2017-10-05 10:33:00 -0700

    question/answer functionality

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const app = express();
 const hb = require('express-handlebars');
+const bodyParser = require('body-parser');
+
+//**** middleware ****//
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // an object that holds the http status codes
 const statusCodes = [
@@ -15,25 +19,50 @@ const statusCodes = [
 app.engine('handlebars', hb({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-//**** routes ****//
-app.get('/', function(req, res) {
-  // randomly select an HTTP status code and display it to the user
+// randomly select an HTTP status code object and return it
+var getCorrect = () => {
   var index = Math.floor(Math.random() * statusCodes.length);
-  var codeObj = statusCodes[index];
+  return statusCodes[index];
+}
+
+// returns an array of four HTTP status code objects that
+// includes the correct answer (passed in as an argument)
+var getChoices = (correct) => {
   // randomly pick four answers to display as multiple choice
   // make sure it includes the correct answer
   var answers = [];
+  answers.push(correct);
   while(answers.length < 4) {
     var index = Math.floor(Math.random() * statusCodes.length);
     var nextChoice = statusCodes[index];
     if (!answers.includes(nextChoice)) {
       answers.push(nextChoice);
-      console.log(nextChoice);
     }
   }
-  res.render('home', {code: codeObj.code, choices: answers});
-})
+  return answers;
+}
+
+//**** routes ****//
+app.get('/', function(req, res) {
+  var correct = getCorrect();
+  var answers = getChoices(correct);
+
+  res.render('home', {correctCode: correct.code, choices: answers});
+});
+
+app.post('/', function(req, res) {
+  // check if answer is correct
+  if (req.body.userAnswer == req.body.correctAnswer) {
+    console.log("correct!");
+  } else {
+    console.log("incorrect!");
+  }
+  // then get new question and display it to the user
+  var correct = getCorrect();
+  var answers = getChoices(correct);
+  res.render('home', {correctCode: correct.code, choices: answers})
+});
 
 app.listen(3000, function(req, res) {
   console.log("listening on port 3000!");
-})
+});
