739e013eb4275a4e07f4bd76dad14af215cb0e22
Catislin <caitlin.estrellita@gmail.com>
2017-10-06 13:46:40 -0700

    added show correct answer

--- index.js
+++ index.js
@@ -9,11 +9,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // an object that holds the http status codes
 const statusCodes = [
+  {code: 100, def: "Continue"},
   {code: 200, def: "OK"},
   {code: 403, def: "Forbidden"},
   {code: 404, def: "Not Found"},
+  {code: 401, def: "Unauthorized"},
+  {code: 423, def: "Locked"},
   {code: 500, def: "Internal Server Error"},
-  {code: 504, def: "Gateway Timeout"}
+  {code: 504, def: "Gateway Timeout"},
+  {code: 511, def: "Network Authentication Required"}
 ];
 
 app.engine('handlebars', hb({defaultLayout: 'main'}));
@@ -23,6 +27,19 @@ app.set('view engine', 'handlebars');
 var getCorrect = () => {
   var index = Math.floor(Math.random() * statusCodes.length);
   return statusCodes[index];
+}
+
+var shuffle = (arr) => {
+  // go through each element
+  for (var i = arr.length - 1; i > 0; i--) {
+    // get a random index in the range
+    var j = Math.floor(Math.random() * (i + 1));
+    // swap the element at that index with the current element
+    var temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+  return arr;
 }
 
 // returns an array of four HTTP status code objects that
@@ -39,7 +56,7 @@ var getChoices = (correct) => {
       answers.push(nextChoice);
     }
   }
-  return answers;
+  return shuffle(answers);
 }
 
 //**** routes ****//
@@ -52,15 +69,16 @@ app.get('/', function(req, res) {
 
 app.post('/', function(req, res) {
   // check if answer is correct
+  var answer;
   if (req.body.userAnswer == req.body.correctAnswer) {
-    console.log("correct!");
+    answer = "Correct!"
   } else {
-    console.log("incorrect!");
+    answer = "Incorrect! The correct answer was: " + req.body.correctDef;
   }
   // then get new question and display it to the user
   var correct = getCorrect();
   var answers = getChoices(correct);
-  res.render('home', {correctCode: correct.code, choices: answers})
+  res.render('home', {correctCode: correct.code, correctDef: correct.def, choices: answers, isCorrect: answer})
 });
 
 app.listen(3000, function(req, res) {
